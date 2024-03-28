10b528f6443c471ee73b62c5bdb56151bb1bc603
Sergio Martins <@>
2024-03-28 20:42:45 +0000

    Remove workspace class
    There's no state to keep.
    Added more tests.

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,6 @@ use clap::Parser;
 
 mod workspace;
 
-use crate::workspace::Workspace;
-
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
@@ -20,10 +18,9 @@ fn suggest_target_filename(template_filename: &str) -> String {
 
 fn main() {
     let args = Args::parse();
-
     let target_filename = suggest_target_filename(&args.filename);
-    let workspace = Workspace::new(args.filename, target_filename);
-    match workspace.generate() {
+
+    match workspace::generate_from_file(args.filename, target_filename) {
         Ok(_) => println!("Workspace generated successfully"),
         Err(e) => {
             eprintln!("Error: {:?}", e);
--- src/workspace.rs
+++ src/workspace.rs
@@ -7,105 +7,80 @@ pub enum Error {
     ExpectedRootObject,
 }
 
-pub struct Workspace {
-    pub template_filename: String,
-    pub target_filename: String,
+pub fn generate_from_file(template_filename: String, target_filename: String) -> Result<(), Error> {
+    // call generate with the contents of the file
+    let template_contents =
+        std::fs::read_to_string(&template_filename).map_err(|e| Error::IoError(e))?;
+
+    let new_json = generate_from_string(&template_contents)?;
+
+    // write json to target file
+    let target_file = std::fs::File::create(&target_filename).map_err(|e| Error::IoError(e))?;
+
+    serde_json::to_writer_pretty(target_file, &new_json).map_err(|e| Error::JsonError(e))?;
+
+    Ok(())
 }
 
-impl Workspace {
-    pub fn new(template_filename: String, target_filename: String) -> Self {
-        Self {
-            template_filename,
-            target_filename,
-        }
+pub fn generate_from_string(template_contents: &String) -> Result<serde_json::Value, Error> {
+    let mut json: serde_json::Value =
+        serde_json::from_str(template_contents).map_err(|e| Error::JsonError(e))?;
+
+    if !json.is_object() {
+        return Err(Error::ExpectedRootObject);
     }
 
-    pub fn generate(&self) -> Result<(), Error> {
-        // call generate with the contents of the file
-        let template_contents =
-            std::fs::read_to_string(&self.template_filename).map_err(|e| Error::IoError(e))?;
+    let globals = json["globals"].as_object().cloned();
+    json.as_object_mut().unwrap().remove("globals");
 
-        let new_json = self.generate_from_string(&template_contents)?;
+    replace_globals(&mut json, &globals)?;
 
-        // write json to target file
-        let target_file =
-            std::fs::File::create(&self.target_filename).map_err(|e| Error::IoError(e))?;
+    Ok(json)
+}
 
-        serde_json::to_writer_pretty(target_file, &new_json).map_err(|e| Error::JsonError(e))?;
-
-        Ok(())
-    }
-
-    pub fn generate_from_string(
-        &self,
-        template_contents: &String,
-    ) -> Result<serde_json::Value, Error> {
-        let mut json: serde_json::Value =
-            serde_json::from_str(template_contents).map_err(|e| Error::JsonError(e))?;
-
-        if !json.is_object() {
-            return Err(Error::ExpectedRootObject);
-        }
-
-        let globals = json["globals"].as_object().cloned();
-        json.as_object_mut().unwrap().remove("globals");
-
-        self.replace_globals(&mut json, &globals)?;
-
-        Ok(json)
-    }
-
-    /// On success, returns whether any globals were replaced (bool).
-    fn replace_globals(
-        &self,
-        value: &mut serde_json::Value,
-        globals: &Option<serde_json::Map<String, serde_json::Value>>,
-    ) -> Result<(), Error> {
-        if value.is_string() {
-            // checks that the value conforms to the format @{contents}
-            let s = value.as_str().unwrap();
-            if s.starts_with("@{") && s.ends_with("}") {
-                let key = &s[2..s.len() - 1];
-                if let Some(globals) = globals {
-                    if let Some(global_value) = globals.get(key) {
-                        *value = global_value.clone();
-                    } else {
-                        println!("No globals found for key: {}", key);
-                    }
+/// On success, returns whether any globals were replaced (bool).
+fn replace_globals(
+    value: &mut serde_json::Value,
+    globals: &Option<serde_json::Map<String, serde_json::Value>>,
+) -> Result<(), Error> {
+    if value.is_string() {
+        // checks that the value conforms to the format @{contents}
+        let s = value.as_str().unwrap();
+        if s.starts_with("@{") && s.ends_with("}") {
+            let key = &s[2..s.len() - 1];
+            if let Some(globals) = globals {
+                if let Some(global_value) = globals.get(key) {
+                    *value = global_value.clone();
                 } else {
                     println!("No globals found for key: {}", key);
                 }
-            }
-            return Ok(());
-        } else if value.is_array() {
-            for v in value.as_array_mut().unwrap() {
-                self.replace_globals(v, globals)?;
-            }
-        } else if value.is_object() {
-            for (_, v) in value.as_object_mut().unwrap() {
-                self.replace_globals(v, globals)?;
+            } else {
+                println!("No globals found for key: {}", key);
             }
         }
-
-        Ok(())
+        return Ok(());
+    } else if value.is_array() {
+        for v in value.as_array_mut().unwrap() {
+            replace_globals(v, globals)?;
+        }
+    } else if value.is_object() {
+        for (_, v) in value.as_object_mut().unwrap() {
+            replace_globals(v, globals)?;
+        }
     }
+
+    Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use serde_json::Value;
+
     use super::*;
 
     #[test]
-    fn test_workspace_ctor() {
-        let filename = "test";
-        let workspace = Workspace::new(String::from("test"), String::from("test2"));
-        assert_eq!(workspace.template_filename, filename);
-    }
-
-    #[test]
     fn test_unknown_file() {
-        let workspace = Workspace::new(String::from("unknown.template"), String::from("unknown"));
-        let result = workspace.generate();
+        let result = generate_from_file("unknown.template".to_string(), "unknown".to_string());
         assert!(result.is_err());
         match result {
             Err(Error::IoError(e)) => match e.kind() {
@@ -129,25 +104,82 @@ mod tests {
             }
         }"#;
 
-        let expected = r#"{
+        let expected: Value = serde_json::from_str(
+            r#"{
             "person": {
                 "name": "John Doe",
                 "age": 42
             }
+        }"#,
+        )
+        .unwrap();
+
+        let result = generate_from_string(&String::from(template)).unwrap();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_list_replacements() {
+        let template = r#"{
+            "globals": {
+                "numbers": [1, 2, 3, 4, 5, 6, 7],
+                "empty": []
+            },
+            "obj": {
+                "l1": "@{numbers}",
+                "l2": "@{empty}"
+            }
         }"#;
 
-        let template_filename = "test.template";
-        let target_filename = "test";
-        let workspace = Workspace::new(
-            String::from(template_filename),
-            String::from(target_filename),
-        );
-        let result = workspace
-            .generate_from_string(&String::from(template))
-            .unwrap();
+        let expected: Value = serde_json::from_str(
+            r#"{
+            "obj": {
+                "l1": [1, 2, 3, 4, 5, 6, 7],
+                "l2": []
+            }
+        }"#,
+        )
+        .unwrap();
 
-        let expected_json: serde_json::Value = serde_json::from_str(expected).unwrap();
+        let result = generate_from_string(&String::from(template)).unwrap();
 
-        assert_eq!(result, expected_json);
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_obj_replacements() {
+        let template = r#"{
+            "globals": {
+                "numbers": {
+                    "one": 1,
+                    "two": 2,
+                    "three": 3
+                },
+                "empty": {}
+            },
+            "obj": {
+                "l1": "@{numbers}",
+                "l2": "@{empty}"
+            }
+        }"#;
+
+        let expected: Value = serde_json::from_str(
+            r#"{
+            "obj": {
+                "l1": {
+                    "one": 1,
+                    "two": 2,
+                    "three": 3
+                },
+                "l2": {}
+            }
+        }"#,
+        )
+        .unwrap();
+
+        let result = generate_from_string(&String::from(template)).unwrap();
+
+        assert_eq!(result, expected);
     }
 }
